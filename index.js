e54c5176aff20b5fdb743ceeda4ad6d7c5f9c120
Dacre <enquiries@mooce.co.nz>
2018-08-06 20:48:37 +1200

    Setup basic  webgl2 rendering

--- index.js
+++ index.js
@@ -1,88 +1,118 @@
-import twgl from './node_modules/twgl.js/dist/3.x/twgl-full.js'
+import { mat4 } from 'gl-matrix'
+import * as shader from './shader'
 
-const m4 = twgl.m4;
-const v3 = twgl.v3;
-const gl = twgl.getWebGLContext(document.getElementById("c"));
-const programInfo = twgl.createProgramInfo(gl, ["vs", "fs"]);
+function initBuffers(gl) {
 
-const textures = twgl.createTextures(gl, {
+  const positionBuffer = gl.createBuffer();
 
-    color : { 
-        mag: gl.NEAREST,
-        min: gl.LINEAR,
-        src: "textures/sand_color.jpg" 
-    },  
-    normal : { 
-        mag: gl.NEAREST,
-        min: gl.LINEAR,
-        src: "textures/sand_normal.jpg" 
-    } 
-})
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-const arrays = {
-  position: [1, 1, -1, 1, 1, 1, 1, -1, 1, 1, -1, -1, -1, 1, 1, -1, 1, -1, -1, -1, -1, -1, -1, 1, -1, 1, 1, 1, 1, 1, 1, 1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, 1, -1, 1, -1, -1, 1, 1, 1, 1, -1, 1, 1, -1, -1, 1, 1, -1, 1, -1, 1, -1, 1, 1, -1, 1, -1, -1, -1, -1, -1],
-  normal:   [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1],
-  texcoord: [1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1],
-  indices:  [0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7, 8, 9, 10, 8, 10, 11, 12, 13, 14, 12, 14, 15, 16, 17, 18, 16, 18, 19, 20, 21, 22, 20, 22, 23],
-};
+  const positions = [
+    -1.0,  1.0,
+     1.0,  1.0,
+    -1.0, -1.0,
+     1.0, -1.0,
+  ];
 
-const bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
+  gl.bufferData(gl.ARRAY_BUFFER,
+                new Float32Array(positions),
+                gl.STATIC_DRAW);
 
-const tex = twgl.createTexture(gl, {
-  min: gl.NEAREST,
-  mag: gl.NEAREST,
-  src: [
-    255, 255, 255, 255,
-    192, 192, 192, 255,
-    192, 192, 192, 255,
-    255, 255, 255, 255,
-  ],
-});
+  return {
+    position: positionBuffer,
+  };
+}
 
-const uniforms = {
-  u_lightWorldPos: [1, 8, -10],
-  u_lightColor: [1, 0.8, 0.8, 1],
-  u_ambient: [0, 0, 0, 1],
-  u_specular: [1, 1, 1, 1],
-  u_shininess: 50,
-  u_specularFactor: 1,
-  u_diffuse: textures.color,
-  u_normal: textures.normal
-};
-
-function render(time) {
-  time *= 0.0075;
-  twgl.resizeCanvasToDisplaySize(gl.canvas);
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
+function drawScene(gl, programInfo, buffers) {
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clearDepth(1.0);
   gl.enable(gl.DEPTH_TEST);
-  gl.enable(gl.CULL_FACE);
+  gl.depthFunc(gl.LEQUAL);
+
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-  const fov = 30 * Math.PI / 180;
+  const fieldOfView = 45 * Math.PI / 180;   // in radians
   const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-  const zNear = 0.5;
-  const zFar = 10;
-  const projection = m4.perspective(fov, aspect, zNear, zFar);
-  const eye = [1, Math.sin(time) * 4, -6];
-  const target = [0, 0, 0];
-  const up = [0, 1, 0];
+  const zNear = 0.1;
+  const zFar = 100.0;
+  const projectionMatrix = mat4.create();
+  mat4.perspective(projectionMatrix,
+                   fieldOfView,
+                   aspect,
+                   zNear,
+                   zFar);
 
-  const camera = m4.lookAt(eye, target, up);
-  const view = m4.inverse(camera);
-  const viewProjection = m4.multiply(projection, view);
-  const world = m4.rotationY(time); //time
+  const modelViewMatrix = mat4.create();
+  mat4.translate(modelViewMatrix,
+                 modelViewMatrix,
+                 [-0.0, 0.0, -6.0]);
 
-  uniforms.u_viewInverse = camera;
-  uniforms.u_world = world;
-  uniforms.u_worldInverseTranspose = m4.transpose(m4.inverse(world));
-  uniforms.u_worldViewProjection = m4.multiply(viewProjection, world);
+  {
+    const numComponents = 2;
+    const type = gl.FLOAT;
+    const normalize = false;
+    const stride = 0;
+    const offset = 0;
+    
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+    gl.vertexAttribPointer(
+        programInfo.attribLocations.vertexPosition,
+        numComponents,
+        type,
+        normalize,
+        stride,
+        offset);
+    gl.enableVertexAttribArray(
+        programInfo.attribLocations.vertexPosition);
+  }
+
+  // Tell WebGL to use our program when drawing
 
   gl.useProgram(programInfo.program);
-  twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
-  twgl.setUniforms(programInfo, uniforms);
-  gl.drawElements(gl.TRIANGLES, bufferInfo.numElements, gl.UNSIGNED_SHORT, 0);
 
-  requestAnimationFrame(render);
+  // Set the shader uniforms
+
+  gl.uniformMatrix4fv(
+      programInfo.uniformLocations.projectionMatrix,
+      false,
+      projectionMatrix);
+  gl.uniformMatrix4fv(
+      programInfo.uniformLocations.modelViewMatrix,
+      false,
+      modelViewMatrix);
+
+  {
+    const offset = 0;
+    const vertexCount = 4;
+    gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+  }
 }
-requestAnimationFrame(render);
+
+async function main() {
+  
+  const canvas = document.querySelector("canvas");
+  const gl = canvas.getContext("webgl2");
+  
+  if (gl === null) {
+    console.error("Unable to initialize WebGL. Your browser or machine may not support it.");
+    return;
+  }
+  
+  const shaderProgram = await shader.loadProgram(gl, '/basic.vs', '/basic.fs');
+  const programInfo = {
+    program: shaderProgram,
+    attribLocations: {
+      vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+    },
+    uniformLocations: {
+      projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
+      modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
+    },
+  };
+
+  const buffers = initBuffers(gl)
+
+  drawScene(gl, programInfo, buffers)
+}
+
+main()
